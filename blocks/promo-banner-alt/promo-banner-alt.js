ab555d741f02ec21b183eed14914979109acb993
MeeraPurohit-adobe <inf68327@adobe.com>
2026-09-02 14:51:04 +0530

    added another promo banner

--- blocks/promo-banner-alt/promo-banner-alt.js
+++ blocks/promo-banner-alt/promo-banner-alt.js
@@ -4,17 +4,17 @@ export default function decorate(block) {
 
   const cols = [...row.querySelectorAll(':scope > div')];
 
-  // text column - left
-  if (cols[0]) cols[0].classList.add('promo-banner-alt-text');
+  // image column - left
+  if (cols[0]) cols[0].classList.add('promo-banner-alt-image');
 
-  // image column - right
-  if (cols[1]) cols[1].classList.add('promo-banner-alt-image');
+  // text column - right
+  if (cols[1]) cols[1].classList.add('promo-banner-alt-text');
 
   // first p = label
-  const label = cols[0]?.querySelector('p:first-child');
+  const label = cols[1]?.querySelector('p:first-child');
   if (label) label.classList.add('promo-banner-alt-label');
 
   // CTA button
-  const cta = cols[0]?.querySelector('a');
+  const cta = cols[1]?.querySelector('a');
   if (cta) cta.classList.add('promo-banner-alt-cta');
 }
